91f00843235cee22711e75ce9a532969f9a8a916
Jacob Klaren <jacob.klaren@outlook.com>
2025-09-05 17:09:34 +0200

    add basics for volume mixer

--- ahkCompanionServer.js
+++ ahkCompanionServer.js
@@ -4,6 +4,9 @@ const { spawn } = require("child_process");
 const path = require("path");
 const os = require("os");
 const fs = require("fs");
+const SoundMixer = require("native-sound-mixer").default;
+const mixer = require("native-sound-mixer");
+const { DeviceType } = mixer;
 
 const app = express();
 app.use(express.json());
@@ -50,6 +53,55 @@ app.post("/runFile", (req, res) => {
 
         ahk.on("exit", code => res.send(`File finished with code ${code}`));
         ahk.stderr.on("data", data => console.error(`AHK Error: ${data}`));
+    } catch (e) {
+        console.error("caught exception", e);
+        res.status(500).send("Server error");
+    }
+});
+
+app.get("/audioSessions", (req, res) => {
+    console.log("got audioSessions call")
+    /* https://www.npmjs.com/package/native-sound-mixer */
+
+    try {
+        const devices = SoundMixer.devices;
+        console.log(devices);
+
+        const device = SoundMixer.getDefaultDevice(DeviceType.RENDER);
+        console.log(device);
+
+        const sessions = device.sessions;
+        const session = sessions[0]; // get the first session for testing
+        console.log(sessions);
+        console.log(session);
+
+        // retrieving the mute flag 
+        const isDeviceMuted = device.mute; // bool
+        console.log(isDeviceMuted);
+
+        /* // toggling mute
+        device.mute = !isDeviceMuted; */
+
+        // retrieving the volume 
+        const deviceVolume = device.volume;
+        console.log(deviceVolume);
+
+        /* // adding 10% to volume
+        device.volume += .1; */
+
+        // set session to a valid session object
+        const isSessionMuted = session.mute;
+        console.log(isSessionMuted);
+        // toggling mute 
+        /* session.mute = !isSessionMuted; */
+
+        // set session to a valid session object
+        const sessionVolume = session.volume;
+        console.log(sessionVolume);
+        // adding 10% to volume
+        /* session.volume += .1; */
+
+        res.json({ devices, device, sessions, isDeviceMuted, deviceVolume, isSessionMuted, sessionVolume });
     } catch (e) {
         console.error("caught exception", e);
         res.status(500).send("Server error");
